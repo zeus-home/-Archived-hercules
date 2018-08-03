99e333eb025af368a72bd1827c282c1be2372116
Swanav <swanav@outlook.com>
2018-08-04 00:01:04 +0530

    Moved mobile_app to a new repository; Edison

--- fan/src/main.cpp
+++ fan/src/main.cpp
@@ -1,9 +1,22 @@
-#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+IPAddress apIP(192, 168, 24, 5);
+ESP8266WebServer webServer(80);
+
+String responseHTML = "{\"foo\":\"bar\"}";
 
 void setup() {
-    // put your setup code here, to run once:
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP("[Hercules]2934FCD34", "IrisConfig");
+
+  webServer.onNotFound([]() {
+    webServer.send(200, "text/html", responseHTML);
+  });
+  webServer.begin();
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  webServer.handleClient();
 }
