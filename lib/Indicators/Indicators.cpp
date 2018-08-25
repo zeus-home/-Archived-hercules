302d9d084a200b05a62806777c2ecd1a909c485d
Swanav <swanav@outlook.com>
2018-08-25 07:59:22 +0530

    Indicator use timer instead of delay.

--- lib/Indicators/Indicators.cpp
+++ lib/Indicators/Indicators.cpp
@@ -2,9 +2,30 @@
 
 #include <Arduino.h>
 
+unsigned long Indicators::previousMillis = 0;
+int Indicators::ledState = LOW;
+
+void Indicators::init() {
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
 void Indicators::blink_symmetric(int led, int duration) {
-    digitalWrite(led, LOW);
-    delay(duration);
-    digitalWrite(led, HIGH);
-    delay(duration);
+
+    unsigned long currentMillis = millis();
+
+    if(currentMillis - previousMillis >= interval) {
+        previousMillis = currentMillis;
+        if(ledState == HIGH) {
+            ledState = LOW;          
+        } else {
+            ledState = HIGH;
+        }
+        digitalWrite(LED_BUILTIN, ledState);
+
+    }
+
+    // digitalWrite(led, LOW);
+    // delay(duration);
+    // digitalWrite(led, HIGH);
+    // delay(duration);
 }
--- lib/Indicators/Indicators.h
+++ lib/Indicators/Indicators.h
@@ -1,9 +1,18 @@
 #ifndef INDICATORS_H
 #define INDICATORS_H
 
+#define interval 1000 
+
 class Indicators {
+
+    static unsigned long previousMillis;
+    static int ledState;
+
     public:
+        static void init();
+        
         static void blink_symmetric(int pin, int duration_milliseconds);
+
 };
 
 #endif
